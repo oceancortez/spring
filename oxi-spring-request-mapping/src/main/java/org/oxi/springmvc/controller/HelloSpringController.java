3b7dbe09d0f7af766cf8a9954aba2548b8a3e7df
Ocean Home <oceancortez@gmail.com>
2017-04-02 13:30:35 -0300

    start

---
+++ oxi-spring-request-mapping/src/main/java/org/oxi/springmvc/controller/HelloSpringController.java
@@ -0,0 +1,42 @@
+package org.oxi.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloSpringController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "hello";
+
+	}
+
+	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+	public ModelAndView hello(@PathVariable("name") String name) {
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("hello");
+		model.addObject("msg", name);
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/rest/{showRest}", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView showRest(@PathVariable("showRest") String showRest){		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("hello");
+		modelAndView.addObject("showRest", showRest);
+		
+		return modelAndView;
+	}
+}
