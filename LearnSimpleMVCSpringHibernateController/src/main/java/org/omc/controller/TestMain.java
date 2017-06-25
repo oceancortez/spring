bf283b4187795e8d70e69b36539dcbb9d8d800af
Ocean Home <oceancortez@gmail.com>
2017-06-25 16:48:57 -0300

    Criando um controller para test

--- LearnSimpleMVCSpringHibernateController/src/main/java/org/omc/controller/TestMain.java
+++ LearnSimpleMVCSpringHibernateController/src/main/java/org/omc/controller/TestMain.java
@@ -3,12 +3,16 @@
  */
 package org.omc.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.omc.util.Paginas;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,6 +30,17 @@ public class TestMain {
 		request.setAttribute("inicio", "Teste OK");
 		
 		return new ModelAndView(Paginas.inicio.forward());
+	}
+	
+	@RequestMapping(value = "testMainResponseBody", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody Usuario testMainResponseBody(HttpServletRequest request){
+		
+		//request.setAttribute("inicio", "Teste OK");
+		Usuario u = new Usuario();
+		u.setNome("Usuario OK");
+		
+		
+		return u;
 	}
 
 }
---
+++ LearnSimpleMVCSpringHibernateController/src/main/java/org/omc/controller/Usuario.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.omc.controller;
+
+/**
+ * @author ocean
+ *
+ */
+public class Usuario {
+	
+	private String nome;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
