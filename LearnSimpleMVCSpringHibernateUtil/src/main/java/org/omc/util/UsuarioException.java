52addc7ada019d7d05761615a6c06e2e2846df9c
T803465 <T803465@AK0275910.tokiomarine.com.br>
2017-07-05 10:35:39 -0300

    ok

---
+++ LearnSimpleMVCSpringHibernateUtil/src/main/java/org/omc/util/UsuarioException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.omc.util;
+
+/**
+ * @author T803465
+ *
+ */
+public class UsuarioException extends Throwable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4397826922127382411L;
+	
+	public UsuarioException(String message) {
+		super(message);
+	}
+
+}
